152ca57c7bc0539f2901d65324415e64f34f025d
gbrough <gbrough@gmail.com>
2021-06-02 14:21:43 -0700

    add subt mult add divide

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
 function add(number1, number2) {
   return number1 + number2;
 }
+function subtract(number1, number2) {
+  return number1 + number2;
+}
+function multiply(number1, number2) {
+  return number1 + number2;
+}
+function divide(number1, number2) {
+  return number1 + number2;
+}
+//(0°C × 9/5) + 32
+function c2f(celsius, fahrenheit){
+  fahrenheit = (celsius * 9/5) + 32;
+  return fahrenheit;
+}
+
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));
+const result = subtract(number1, number2);
+alert(result);
